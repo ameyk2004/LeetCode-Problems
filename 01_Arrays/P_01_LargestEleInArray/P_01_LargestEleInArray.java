260a68ac0a4fa5b709c9df5a81292208c2b37a7e
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2025-04-19 16:47:07 +0530

    Problem 1

---
+++ 01_Arrays/P_01_LargestEleInArray/P_01_LargestEleInArray.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class P_01_LargestEleInArray {
+
+    public static int findLargestNumber(int arr[]){
+
+        Arrays.sort(arr);
+
+        return arr[arr.length-1];
+
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {10, 6, 5, 3, 2, 102};
+
+        System.out.println("Largest Number is : " + findLargestNumber(arr));
+    }
+
+}
